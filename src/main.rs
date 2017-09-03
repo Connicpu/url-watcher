a3d89368d59e2d75b1a6efb42b9e02f0215e7b56
Connie Hilarides <conni_h@outlook.com>
2017-09-03 15:22:49 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+extern crate reqwest;
+
+use std::io::Read;
+use std::process::exit;
+use std::mem::swap;
+use std::thread;
+
+fn usage(code: i32) -> ! {
+    eprintln!("Usage: url-watcher [frequency (ms)] [watched url] [trigger url]");
+    exit(code);
+}
+
+fn main() {
+    let mut args = std::env::args();
+    args.next();
+
+    let mut first_arg = args.next().unwrap_or_else(|| usage(0));
+    if first_arg == "--help" || first_arg == "-h" {
+        usage(0);
+    }
+
+    let mut verbose = false;
+    if first_arg == "-v" {
+        verbose = true;
+        first_arg = args.next().unwrap_or_else(|| usage(0));
+    }
+
+    let frequency = first_arg
+        .parse()
+        .map(std::time::Duration::from_millis)
+        .unwrap_or_else(|_| usage(1));
+
+    let watched_url = args.next().unwrap_or_else(|| usage(1));
+
+    let trigger_url = args.next().unwrap_or_else(|| usage(1));
+
+    let mut body = vec![];
+    let mut prev_body = vec![];
+
+    let mut resp = match reqwest::get(&watched_url) {
+        Ok(resp) => resp,
+        Err(_) => {
+            eprintln!("Failed to request URL to obtain the initial body");
+            exit(1);
+        }
+    };
+    resp.read_to_end(&mut prev_body)
+        .expect("Failure to read initial request body");
+    drop(resp);
+
+    let mut consecutive_failures = 0;
+    let mut do_post_check = false;
+    loop {
+        thread::sleep(frequency);
+
+        let mut resp = match reqwest::get(&watched_url) {
+            Ok(resp) => resp,
+            Err(_) => {
+                consecutive_failures += 1;
+                if consecutive_failures < 10 {
+                    eprintln!("Failed to request watched URL, will try again");
+                    continue;
+                } else {
+                    eprintln!("Failed to request watched URL 10 times in a row");
+                    std::process::exit(1);
+                }
+            }
+        };
+
+        resp.read_to_end(&mut body)
+            .expect("IO failure reading web request");
+
+        if !do_post_check && body != prev_body {
+            println!("Payload bodies differ, requesting target URL");
+            reqwest::get(&trigger_url).expect("Trigger URL failure");
+            do_post_check = true;
+        } else {
+            if verbose && !do_post_check {
+                println!("Bodies are the same");
+            }
+            do_post_check = false;
+        }
+
+        swap(&mut body, &mut prev_body);
+        body.clear();
+    }
+}
